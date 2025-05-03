e0ffc38c36e2fc3186dcca22f2431de6f8fc5a89
Timothy Johnson II <timothyallenjohnsonii@gmail.com>
2025-05-03 00:38:56 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,4 @@ def home():
     return "Hello, Flask is working!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
