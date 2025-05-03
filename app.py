cfa7585805297f38fa27b34403a58d0f5cbcaa8d
Timothy Johnson II <timothyallenjohnsonii@gmail.com>
2025-05-03 01:25:45 -0400

    Auto sync update

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 import nmap
 
 app = Flask(__name__)
 
+def scan_network():
+    nm = nmap.PortScanner()
+    nm.scan(hosts="192.168.1.0/24", arguments="-sn")
+    hosts = []
+
+    for host in nm.all_hosts():
+        hosts.append({
+            'ip': host,
+            'hostname': nm[host].hostname(),
+            'mac': nm[host]['addresses'].get('mac', 'N/A')
+        })
+
+    return hosts
+
 @app.route("/")
 def home():
-    nm = nmap.PortScanner()
-    nm.scan(hosts='192.168.1.0/24', arguments='-sn')  # Ping scan
-
-    results = []
-    for host in nm.all_hosts():
-        hostname = nm[host].hostname()
-        mac = nm[host]['addresses'].get('mac', 'N/A')
-        results.append(f"Host: {host} ({hostname}) | MAC: {mac}")
-
-    return "<br>".join(results)  # Render results in HTML line breaks
+    results = scan_network()
+    return render_template("scan_results.html", results=results)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
